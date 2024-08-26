6baf29d383410927bcb3801b965a24cf0b417846
David Lopez Ramirez <jesdav18@outlook.es>
2024-08-25 18:42:09 -0600

    New: Agregando dto para transportar información en la validación de dominio de cualquier feature

---
+++ Nagaira.Template.Api/Features/Shared/Domain/ValidatorDomainDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Nagaira.Template.Api.Features.Shared.Domain
+{
+    public class ValidatorDomainDto
+    {
+        public bool IsValid { get; set; }
+        public string? Message { get; set; }
+    }
+}
